1cf11a8985310caa1cfaecc4d71eac8d8a80aba9
bertramd <dylan.bertram@medforall.com>
2017-07-12 15:00:51 -0400

    Add files via upload

---
+++ LRNG_xp3_Blink.ino
@@ -0,0 +1,17 @@
+int LEDPin = 13;          //initializes LEDPin as the pin the LED is connected to
+int waitTimeOn = 1000;    //initializes a variable that sets the time the LED is on
+int waitTimeOff = 1000;   //initializes a variable that sets the time the LED is off
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LEDPin, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LEDPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(waitTimeOn);            // how long the LED is on
+  digitalWrite(LEDPin, LOW);    // turn the LED off by making the voltage LOW
+  delay(waitTimeOff);           // how long the LED is off
+}
